0b00bc14b386e53f50b358daafd5c1431bfe4b36
AnYuan <Journeyerrr@gmail.com>
2021-08-19 11:29:25 +0800

    controller 测试

---
+++ src/test/java/com/zayan/www/controller/test/SkuTestControllerTest.java
@@ -0,0 +1,34 @@
+package com.zayan.www.controller.test;
+
+import org.hamcrest.core.StringContains;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SkuTestControllerTest {
+
+    @Autowired
+    private  MockMvc mvc;
+
+    @Test
+    public void skuTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/test/sku/2")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.content().string(StringContains.containsString("8001")))
+                .andReturn();
+    }
+}
